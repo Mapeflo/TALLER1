84e1758490a8126ef97ef279df806bc7bac9736a
mapeflo <mpenarandaf@unicartagena.edu.co>
2025-09-05 20:49:02 -0500

    Se inicia el ejercicio final (proyecto integrador). Los metodos se mueven a la carpeta del main.

--- src/taller1_1/Taller1_1.java
+++ src/taller1_1/Taller1_1.java
@@ -14,7 +14,16 @@ public class Taller1_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+    Libro libro1 = new Libro ();   
+    Libro libro2 = new Libro ("Cien años de soledad","Garcia Marquez", 500);
+    
+    CuentaBancaria cuenta1 = new CuentaBancaria ();
+    CuentaBancaria cuenta2 = new CuentaBancaria (50465847, 100000, "Corriente");
+    
+    Estudiante estudiante1 = new Estudiante ();
+    Estudiante estudiante2 = new Estudiante ("Maria", 16, "11-D");
+    
     }
     
 }
